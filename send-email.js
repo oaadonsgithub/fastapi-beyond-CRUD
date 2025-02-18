e00a1c0da6957f4def01d710b24ca3d7283dd311
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-02-18 09:32:21 -0800

    Update send-email.js

--- send-email.js
+++ send-email.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
     to: 'oaaderibigbe@usfca.edu',
-    from: 'oaausc@gmail.com,
+    from: 'oaausc@gmail.com',
     subject: 'Hello world',
     text: 'Hello plain world!',
     html: '<p>Hello HTML world!</p>',
